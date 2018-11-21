68fa90f08514de82dc79d610867f301ca0defcab
Gerd Aschemann <gerd@aschemann.net>
2018-11-21 16:17:19 +0100

    Fix Controller -> RestController

--- src/main/java/com/e4developer/kafkaintro/KafkaWebController.java
+++ src/main/java/com/e4developer/kafkaintro/KafkaWebController.java
@@ -2,12 +2,12 @@ package com.e4developer.kafkaintro;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Slf4j
 public class KafkaWebController {
 
@@ -17,7 +17,6 @@ public class KafkaWebController {
     @PostMapping("/kafka/{topicName}")
     public String sendToTopic(@PathVariable String topicName, @RequestBody String message) {
         log.info ("Sending '{}' to topic '{}'", message, topicName);
-
         kafkaSender.send(topicName, message);
         return "Message sent";
     }
