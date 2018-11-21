9fb7ff87aec03e77c8af6a88eccb22f32a9d98d3
Gerd Aschemann <gerd@aschemann.net>
2018-11-21 16:16:57 +0100

    Use lombok/Slf4j for Receiver

--- src/main/java/com/e4developer/kafkaintro/KafkaReceiver.java
+++ src/main/java/com/e4developer/kafkaintro/KafkaReceiver.java
@@ -1,20 +1,22 @@
 package com.e4developer.kafkaintro;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class KafkaReceiver {
 
     @KafkaListener(topics = "topic1")
     public void receiveTopic1(ConsumerRecord<?, ?> consumerRecord) {
-        System.out.println("Receiver on topic1: "+consumerRecord.toString());
+        log.info("Receiver on topic1: '{}'", consumerRecord);
     }
 
     @KafkaListener(topics = "topic2")
     public void receiveTopic2(ConsumerRecord<?, ?> consumerRecord) {
-        System.out.println("Receiver on topic2: "+consumerRecord.toString());
+        log.info("Receiver on topic2: '{}'", consumerRecord);
     }
 
 }
